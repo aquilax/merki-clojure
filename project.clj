b5e92d3d3c203779f8a9a2b07921a61bc1a5b57b
aquilax <aquilax@gmail.com>
2018-06-24 21:04:16 +0200

    Started implementation

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "https://github.com/aquilax/merki-clojure"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [
+    [org.clojure/clojure "1.8.0"]
+    [clj-sub-command "0.4.1"]
+    [org.clojure/tools.cli "0.3.7"]
+  ]
   :main ^:skip-aot merki.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/merki/core.clj
+++ src/merki/core.clj
@@ -1,7 +1,42 @@
 (ns merki.core
+  (:require
+   [clojure.string :as string]
+   [clj-sub-command.core :refer [parse-cmds]]
+   [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (string/join \newline errors)))
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
+
+(def options
+  [["-h" "--help"]])
+
+(def commands
+  [["add" "Add new measurement"]])
+
+(defn usage [options-summary commands-summary]
+  (->> ["Usage: program-name [--help] <command> [<args>]"
+        ""
+        "Options:"
+        options-summary
+        ""
+        "Commands:"
+        commands-summary]
+       (string/join \newline)))
+
+(defn add [args]
+  (println args))
+
+(defn -main [& args]
+  (let [{:keys [options command arguments errors options-summary commands-summary]}
+        (parse-cmds args options commands)]
+    (cond
+      (:help options) (exit 0 (usage options-summary commands-summary))
+      errors (exit 1 (error-msg errors)))
+    (case command
+      :add (add arguments))))
